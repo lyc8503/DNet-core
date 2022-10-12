39335d4b4b75fcce798578dd6ccdbf98a67ff0e4
lyc8503 <lyc8503@foxmail.com>
2022-10-12 17:17:23 +0800

    some L2 definitions

--- ethernet/EthernetLayer.h
+++ ethernet/EthernetLayer.h
@@ -9,14 +9,28 @@
 #include "../driver/driver.h"
 
 
+#pragma pack(1)
+// https://en.wikipedia.org/wiki/Ethernet_frame
+struct EthernetFrame {
+    uint8_t dest_mac[6];
+    uint8_t src_mac[6];
+    uint8_t ether_type[2];
+    uint8_t payload_and_checksum[0];
+};
+#pragma pack()
+
+enum EtherType {
+    ARP = 0x0806,
+    IP = 0x0800
+};
+
 class EthernetLayer: Layer{
 
 public:
-    EthernetLayer(driver dri);
+    explicit EthernetLayer();
 
-    virtual ssize_t send(void* buf, size_t size) = 0;
-//    virtual ssize_t on_recv(void* buf, size_t size) = 0;
-
+    ssize_t send(void* buf, size_t size) override;
+    void on_recv(void* buf, size_t size) override;
 };
 
 
---
+++ util/util.h
@@ -0,0 +1,12 @@
+//
+// Created by lyc8503 on 2022/10/12.
+//
+
+#ifndef DNET_UTIL_H
+#define DNET_UTIL_H
+
+#include <cstdint>
+
+void print_hex(uint8_t* buf, int size);
+
+#endif //DNET_UTIL_H
