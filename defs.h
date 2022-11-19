3cbe4b1d57abf75d30ad1447b1e9502812a56127
lyc8503 <admin@ly8503.site>
2022-11-19 22:36:09 +0800

    better assert: supports msg and throw runtime error.

--- defs.h
+++ defs.h
@@ -17,6 +17,25 @@
 //#define DNET_DEBUG(format, args...) void(0);
 //#define DNET_ERROR(format, args...) void(0);
 
-#define DNET_ASSERT assert
+/**
+ *  Overload for Macro functions(DNET_ASSERT): https://stackoverflow.com/questions/16683146/can-macros-be-overloaded-by-number-of-arguments
+ */
+#define CAT( A, B ) A ## B
+#define SELECT( NAME, NUM ) CAT( NAME ## _, NUM )
+
+#define GET_COUNT( _1, _2, _3, _4, _5, _6 /* ad nauseam */, COUNT, ... ) COUNT
+#define VA_SIZE( ... ) GET_COUNT( __VA_ARGS__, 6, 5, 4, 3, 2, 1 )
+
+#define VA_SELECT( NAME, ... ) SELECT( NAME, VA_SIZE(__VA_ARGS__) )(__VA_ARGS__)
+
+#define DNET_ASSERT( ... ) VA_SELECT( DNET_ASSERT, __VA_ARGS__ )
+
+#define DNET_ASSERT_2(expr, msg) do {                                                                                                                   \
+    if (!(expr)) {                                                                                                                                      \
+        throw std::runtime_error("Assertion failed at [" + std::string(__FILENAME__) + ":" + std::to_string(__LINE__) + "]: " + std::string(msg));      \
+    }                                                                                                                                                   \
+} while(0)
+
+#define DNET_ASSERT_1(expr) DNET_ASSERT_2(expr, "No msg provided.")
 
 #endif //DNET_DEFS_H
