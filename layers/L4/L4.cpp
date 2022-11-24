26f7fcc46a455ad05f110b2e87adcf08f0122edc
lyc <lyc@localhost>
2022-11-24 07:27:32 +0000

    L4 init (vim is too hard to use)

---
+++ layers/L4/L4.cpp
@@ -0,0 +1,15 @@
+//
+// Created by lyc8503 on 2022/10/24.
+//
+
+
+#include "L4.h"
+
+L4::L4(DNet& context): context(context) {
+
+}
+
+void L4::on_recv(void* buf, size_t size, IPV4_PROTOCOL protocol) {
+
+}
+
